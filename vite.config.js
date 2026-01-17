ec1e3c4f236245f232187a541597c608a1ae3fa5
Matin Imam <matinimam77@gmail.com>
2026-01-17 21:26:39 +0530

    updated config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-export default defineConfig({
+// https://vite.dev/config/
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: "/TaskFlow-To-do/",
-});
+  base: mode === "production" ? "/TaskFlow-To-do/" : "/",
+}));
